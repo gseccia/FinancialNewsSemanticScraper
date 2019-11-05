9344947729372eafc02b3dd22f23da3b38541d36
Anto9696 <antonio.vicinanza96@gmail.com>
2019-11-05 10:08:04 +0100

    Financial scraper upodate

--- financial_web_scraper.py
+++ financial_web_scraper.py
@@ -1,25 +1,86 @@
 from bs4 import BeautifulSoup
 import requests
-from datetime import date
-
+import datetime
+import csv
+import time
+import os.path
+import re
 
 def retrieve_news(news_URL = "https://finviz.com/news.ashx"):
     """ Returns a list of news or None if there aren't """
     request = requests.get(news_URL)
     if request.status_code == 200:
         parser = BeautifulSoup(request.text, "html.parser")
-        # Select only NEWS from table and ignore BLOGS
-        news = parser.find_all("div", class_="news")[0].findChildren()[1].findChildren("tr", recursive=False)[1].\
-            findChildren("td", recursive=False)[0].findChildren("a", class_="nn-tab-link")
-        return list(map(lambda elem: elem.text,news))
+        news_table = parser.select(".news > table:nth-child(2) > tr:nth-child(2) > td > table")[0]
+        news = []
+        for rows in news_table.find_all("tr",class_="nn"):
+            news.append({"date":None,"text":None,"link":None})
+            news[-1]["link"] = re.findall("http.*',",rows.attrs["onclick"])[0][:-2]
+            for col in rows.find_all("td"):
+                if col.has_attr("title"):
+                    news[-1]["text"] = col.a.text
+                if "class" in col.attrs and "nn-date" in col.attrs["class"]:
+                    news[-1]["date"] = col.text
+        return news
     else:
         return None
 
-
-if __name__ == "__main__":
+def autoretrieve_news():
+    # retrieve news
     news = retrieve_news()
 
-    with open("news" + date.today().strftime("%d%m%Y") + ".txt", "w") as f:
-        for n in news:
-            f.write(n+"\n")
-        f.close()
+    # Select today news
+    current_time = datetime.datetime.now()
+    today_news = []
+    fresh_news = []
+    today = datetime.date.today()
+    today_sec = (datetime.datetime(today.year,today.month,today.day)-datetime.datetime(1970,1,1)).total_seconds() - 2*60*60
+    for row in news:
+        try:
+            date_time = datetime.datetime.strptime(row["date"],"%I:%M%p")
+            total_sec = (date_time-datetime.datetime(1900,1,1)).total_seconds() + today_sec
+            date_time = datetime.datetime.fromtimestamp(total_sec)
+            row["date"] = date_time
+            today_news.append(row)
+        except Exception as e:
+            pass
+
+    # Sorting by datetime
+    today_news = sorted(today_news,key=(lambda row: row["date"]))
+
+    # Get the datetime of last saved news and select only fresh news
+    if os.path.exists("news.csv"):
+        # Check the last news saved
+        with open("news.csv","r") as f:
+            csv_reader = csv.DictReader(f)
+            for row in csv_reader:
+                last_date = datetime.datetime.strptime(row["date"],"%Y-%m-%d %H:%M:%S")
+            for row in today_news:
+                if row["date"] > last_date:
+                    fresh_news.append(row)
+            newfile = False
+    else:
+        fresh_news = today_news
+        newfile = True
+                    
+    
+    # Update the news
+    with open('news.csv', mode='a+') as csv_file:
+        fieldnames = ['date', 'text', 'link']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        if newfile:
+            writer.writeheader()
+        for row in fresh_news:
+            writer.writerow(row)
+        csv_file.close()
+
+    return fresh_news
+    
+if __name__=="__main__":
+    while True:
+        try:
+            autoretrieve_news()
+            print("Acquisition at ",datetime.datetime.now(), "SUCCESS")
+        except:
+            print("Acquisition at ",datetime.datetime.now(), "FAILED")
+        time.sleep(5*60)

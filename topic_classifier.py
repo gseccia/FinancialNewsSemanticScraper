3015d7cb72ffb825cb2a7a6a69a267d2119d2935
JBFourierous <marco.carpentiero@gmail.com>
2019-11-09 09:13:40 +0100

    basic structure for ML classifier

---
+++ topic_classifier.py
@@ -0,0 +1,12 @@
+class TopicClassifier():
+
+    """ Class constructor """
+    def __init__(self):
+        pass
+
+    """ Classifies a news topic according the the macro-topics EconomicsTopics and OtherTopics, and the specific
+    economics topics CompaniesEconomy, Markets&Goods, NationalEconomy
+    @:param news title of the news to classify
+    @:return tuple indicating the specific topic of the news and the macro-topic """
+    def classify_news(self, news: str) -> tuple:
+        pass
